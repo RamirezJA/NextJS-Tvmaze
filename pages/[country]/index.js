c01c493e696f1116b856cca8b42c1732443c4dc0
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-08-31 23:40:29 -0700

    12

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,3 +1,14 @@
-const CountryTest = () => <h1>COuntry test</h1>;
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const CountryTest = () => {
+  useEffect(() => {
+    axios
+      .get('http://api.tvmaze.com/schedule?country=US&date=2014-12-01')
+      .then((response) => console.log(response.data));
+  }, []);
+
+  return <h1>This Country test!</h1>;
+};
 
 export default CountryTest;
