03dd47de7a1d889640a7a04b3a4c232c7881b5a8
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-02 23:34:46 -0700

    19start

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,8 +1,20 @@
 const Thumbnail = ({ imageUrl, caption }) => {
-  return <div className="thumbnail">
-      <img src={imageUrl} alt="thumbnail_image"/>
-<h3 className="thumbnail_caption">{caption}</h3>
-  </div>;
+  return (
+    <div className="thumbnail">
+      <img src={imageUrl} alt="thumbnail_image" />
+      <h4 className="thumbnail_caption">{caption}</h4>
+
+      <style jsx>{`
+        .thumbnail_image {
+          width: 100%;
+        }
+
+        .thumbnail_caption {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Thumbnail;
