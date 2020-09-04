2e2d4f5562a556efc23e5955b105a8e3f04f250a
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-03 23:54:21 -0700

    19

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,18 +1,12 @@
+import ThumbnailStyles from './styles';
+
 const Thumbnail = ({ imageUrl, caption }) => {
   return (
     <div className="thumbnail">
       <img src={imageUrl} alt="thumbnail_image" />
       <h4 className="thumbnail_caption">{caption}</h4>
 
-      <style jsx>{`
-        .thumbnail_image {
-          width: 100%;
-        }
-
-        .thumbnail_caption {
-          text-align: center;
-        }
-      `}</style>
+      <style jsx>{ThumbnailStyles}</style>
     </div>
   );
 };
---
+++ components/Thumbnail/styles.js
@@ -0,0 +1,15 @@
+import css from 'styled-jsx/css';
+import Thumbnail from '.';
+
+const ThumbnailStyles = css`
+  .thumbnail_image {
+    width: 100%;
+  }
+
+  .thumbnail_caption {
+    text-align: center;
+    padding: 10px;
+  }
+`;
+
+export default ThumbnailStyles;
