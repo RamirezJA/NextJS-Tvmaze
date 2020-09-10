0ae607b648fec2f9515d3b45349a7db2b869acef
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-09 23:24:54 -0700

    32d

--- components/Cast/index.js
+++ components/Cast/index.js
@@ -3,7 +3,7 @@ import Thumbnail from '../Thumbnail';
 const Cast = ({ cast }) => {
   const renderCast = () => {
     return cast.map((castItem, index) => {
-      const { image, name } = castItem;
+      const { image, name } = castItem.person;
 
       return (
         <li key={index}>
@@ -20,6 +20,16 @@ const Cast = ({ cast }) => {
     <div className="cast">
       <h3>Cast</h3>
       <ul className="cast__list">{renderCast()}</ul>
+      <style jsx>
+        {`
+          .cast__list {
+            display: flex;
+            padding: 0;
+            margin: 0;
+            list-style-type: none;
+          }
+        `}
+      </style>
     </div>
   );
 };
