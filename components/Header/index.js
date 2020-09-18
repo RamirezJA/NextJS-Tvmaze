eb76ed351572b994eeeb022f58df94bd8a4ab187
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-18 08:57:46 -0700

    37 pg :bento:

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,33 @@
+const countries = [{
+    label: 'us',
+    name: 'United States'
+}, {
+    label: 'br',
+    name: 'Brazil'
+}]
+
+
 const Header = () => {
+
+    const handleChange = (e) => {
+        console.log('selected country: ', e.target.value)
+    }
+
+    const renderCountries = () => {
+        return countries.map(country => {
+            return (
+            <option key={country.label} value={country.label}> {country.name}</option>
+            )
+        })
+    }
+
+
     return (
     <div className='header'>
-        Header
-    <style jsx>{`
+        <select onChange={handleChange}>
+            { renderCountries()}
+        </select>
+        <style jsx>{`
         .header {
             padding: 20px;
             background-color: #333;
