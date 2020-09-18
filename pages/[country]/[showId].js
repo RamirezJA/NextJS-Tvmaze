e3548570ec63aa7998f7b195e8fc2df9a562f6c6
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-18 10:23:08 -0700

    43 :goal_net:

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import parse from 'html-react-parser';
 import Cast from '../../components/Cast';
+import Error from 'next/error';
 
-const ShowDetails = ({ show }) => {
+const ShowDetails = ({ show = {}, statusCode }) => {
   const { name, image, summary, _embedded } = show;
+
+  if(statusCode){
+    return <Error statusCode={statusCode} title='Ooops there was a problem.../>
+  }
 
   return (
     <div className="show-details">
@@ -25,11 +30,19 @@ const ShowDetails = ({ show }) => {
 };
 
 ShowDetails.getInitialProps = async ({ query }) => {
-  const { showId } = query;
+  try {
+    const { showId } = query;
   const response = await axios.get(`http://api.tvmaze.com/shows/${showId}?embed=cast`);
   return {
     show: response.data,
   };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500
+    }
+  }
+
+
 };
 
 export default ShowDetails;
